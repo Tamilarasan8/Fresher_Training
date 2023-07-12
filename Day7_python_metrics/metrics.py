00dca77d3a54ee3f4877c84c717235cb5a8cdeb6
Tamilarasan8 <tamilarasan200289@gmail.com>
2023-07-12 17:48:18 +0530

    days

--- Day7_python_metrics/metrics.py
+++ Day7_python_metrics/metrics.py
@@ -1,6 +1,7 @@
 from sklearn import metrics
 import matplotlib.pyplot as plot
 import numpy as np
+
 D='Dog'
 C='Cat'
 R='Rat'
@@ -15,3 +16,18 @@ print(f'Precision : {metrics.precision_score(y_test,y_pred)}')
 print(f'Recall : {metrics.recall_score(y_test,y_pred)}')
 print(f'F1 score : {metrics.f1_score(y_test,y_pred)}')
 print(f'Roc_Auc : {metrics.roc_auc_score(y_test,prop)}')
+
+from nltk.translate.bleu_score import sentence_bleu
+from nltk.metrics import edit_distance
+reference=[['this','is','text','form']]
+candidate=['this','is','text']
+print("bleu score",sentence_bleu(reference,candidate,weights=(1,0,0)))
+print("bleu score",sentence_bleu(reference,candidate,weights=(0,1,0)))
+print("bleu score",sentence_bleu(reference,candidate,weights=(0,0,1)))
+
+var1="Hello world"
+var2="Hi everyone there"
+print('character error : ',edit_distance(var1,var2)/len(var1))
+var3="Hello world".split()
+var4="Hi everyone there".split()
+print('character error : ',edit_distance(var1,var2)/len(var1))
---
+++ Day8_python_metrics_continue/metrics.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+df=pd.read_csv('ex-metrics.csv')
+x=df.iloc[:,2]
+y=df.iloc[:,-1]
+# print(f'Precision : {metrics.precision_score(x,y)}')
+print(f'Roc_Auc : {metrics.roc_auc_score(y,x)}')
+one_hot_encoded_data = pd.get_dummies(df, columns = ['segments', 'stdev'])
+print(one_hot_encoded_data)
